5e94719dea3ecfa9e4ae0938d8874bbfa1a196e6
raccoonek <122537457+raccoonek@users.noreply.github.com>
2023-05-28 21:33:39 +0300

    Add files via upload

---
+++ ClassLibrary_get_dictionary/ClassLibrary_get_dictionary/Class1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_get_dictionary
+{
+    public class ClassLibrary
+    {
+        const string UnwantedSigns = ".,?!-/*”“'\"";
+        private static Dictionary<string, int> get_dictionary(string text)
+        {
+                string FileContents = text;
+                foreach (char c in UnwantedSigns)
+                {
+                    FileContents = FileContents.Replace(c, ' ');
+                    GC.Collect();
+                }
+                FileContents = FileContents.Replace(Environment.NewLine, " ");
+                string[] Words = FileContents.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                FileContents = string.Empty;
+                GC.Collect();
+                Dictionary<string, int> WordCounts = new Dictionary<string, int>();
+                foreach (string w in Words.Select(x => x.ToLower()))
+                    if (WordCounts.TryGetValue(w, out int c))
+                        WordCounts[w] = c + 1;
+                    else
+                        WordCounts.Add(w, 1);
+                Words = new string[1];
+                GC.Collect();
+                return WordCounts;
+        }
+        public static Dictionary<string, int> get_dictionary_assync(string text)
+        {
+            
+            string FileContents = text;
+            Task[] tasks = new Task[11];
+            int i = 0;
+            foreach (char c in UnwantedSigns)
+            {
+                tasks[i] = Task.Factory.StartNew(() => {
+                    FileContents = FileContents.Replace(c, ' ');
+                    
+                });
+                i++;
+                
+            }
+            GC.Collect();
+            Task.WaitAll(tasks);
+            FileContents = FileContents.Replace(Environment.NewLine, " ");
+            string[] Words = FileContents.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            FileContents = string.Empty;
+            GC.Collect();
+            Dictionary<string, int> WordCounts = new Dictionary<string, int>();
+
+            
+            foreach (string w in Words.Select(x => x.ToLower()))
+            {
+                
+                    if (WordCounts.TryGetValue(w, out int c))
+                        WordCounts[w] = c + 1;
+                    else
+                        WordCounts.Add(w, 1);
+                
+            }
+                
+            Words = new string[1];
+            GC.Collect();
+            
+
+            return WordCounts;
+        }
+        
+        public static Dictionary<string, int> get_dictionary_ThreadPool(string text)
+        {
+            string FileContents = text;
+            
+            foreach (char c in UnwantedSigns)
+            {
+                ThreadPool.QueueUserWorkItem(a => {
+                    FileContents = FileContents.Replace(c, ' ');
+                    
+                });
+                
+            }
+            GC.Collect();
+            FileContents = FileContents.Replace(Environment.NewLine, " ");
+            string[] Words = FileContents.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            FileContents = string.Empty;
+            GC.Collect();
+            Dictionary<string, int> WordCounts = new Dictionary<string, int>();
+            foreach (string w in Words.Select(x => x.ToLower()))
+                if (WordCounts.TryGetValue(w, out int c))
+                  WordCounts[w] = c + 1;
+                else
+                  WordCounts.Add(w, 1);
+            
+                
+            Words = new string[1];
+            GC.Collect();
+            return WordCounts;
+        }
+    }
+}
